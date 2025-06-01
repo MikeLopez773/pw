4cb99413e8bdd54541b033b26237774ccb64e81a
TiagoPires11 <121346353+TiagoPires11@users.noreply.github.com>
2025-06-01 21:47:44 +0100

    Update user.js

--- src/models/user.js
+++ src/models/user.js
@@ -2,10 +2,15 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
   username: { type: String, required: true, unique: true },
+  email:    { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  email: { type: String, required: true, unique: true },
-  role: { type: String, default: 'user' },
-  isLoggedIn: { type: Boolean, default: false } // <-- NOVO CAMPO
+  role:     { type: String, enum: ['admin', 'tecnico', 'gestor', 'cliente'], required: true },
+  mfa: {
+    secret: String,
+    enabled: { type: Boolean, default: false }
+  },
+  panels: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Panel' }],
+  isLoggedIn: { type: Boolean, default: false }
 });
 
 module.exports = mongoose.model('User', userSchema);
